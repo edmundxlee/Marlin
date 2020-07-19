8c9cfd938c5392b73dfcd5b8fba654adca3569da
Edmund <edmundxlee@users.noreply.github.com>
2020-07-20 01:20:10 +0800

    reduced linear advance further

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1593,7 +1593,7 @@
 #define LIN_ADVANCE // twoflyingfox
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.84    // Unit: mm compression per 1mm/s extruder speed (twoflyingfox: oriignal 0.22)
+  #define LIN_ADVANCE_K 0.68    // Unit: mm compression per 1mm/s extruder speed (twoflyingfox: oriignal 0.22, calibrated 0.84, but seems a bit off. dropped to 0.74. Still seems off, so dropped to 0.68.)
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
   #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
 #endif
